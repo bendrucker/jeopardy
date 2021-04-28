d10014d49d1430437250f71bbef7abbadce849a1
Noah Hummel <strangedev@posteo.net>
2021-04-28 14:03:24 +0200

    feat: add linux support by detecting various widely-used players

--- index.js
+++ index.js
@@ -4,17 +4,35 @@ const child = require('child_process')
 const eos = require('end-of-stream')
 const wrap = require('spawn-wrapper')
 const path = require('path')
+const shell = require('shelljs')
 const jeopardy = path.resolve(__dirname, 'jeopardy.mp3')
 
+const players = {
+  'afplay': (audioFile) => ['afplay', [ audioFile ]],
+  'mplayer': (audioFile) => ['mplayer', [ audioFile ]],
+  'mpv': (audioFile) => ['mpv', [ '--no-audio-display', audioFile]],
+  'vlc': (audioFile) => ['vlc',[ '-I', 'rc',  audioFile ]]
+}
+
+const findPlayer = function () {
+  for (const playerName of Object.keys(players)) {
+    const { code } = shell.exec(`which ${playerName}`, { silent: true })
+
+    if (code === 0) {
+      return players[playerName]
+    }
+  }
+
+  return () => [ 'echo' ]
+}
+
 module.exports = function run (command, args, callback) {
+  const getPlayerCommand = findPlayer()
   const main = child.spawn(command, args, {
     stdio: 'inherit'
   })
 
-  wrap(main, [
-    'afplay',
-    [jeopardy]
-  ])
+  wrap(main, getPlayerCommand(jeopardy))
 
   eos(main, callback)
 }
